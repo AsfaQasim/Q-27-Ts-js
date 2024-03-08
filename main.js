4c64c23f1b083fcd37b7adedd0d50cd87c565638
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 08:40:52 -0800

    Q 27 of Ts/js &  node js of 45 assignment.
    Q 27 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,13 @@
+var alienColor = "red";
+if (alienColor == "green") {
+    console.log("the player earned 5 points");
+}
+else if (alienColor == "Yellow") {
+    console.log("the player earned 10 points");
+}
+else if (alienColor == "red") {
+    console.log("the player earned 15 points");
+}
+else {
+    console.log("the player earned 0 points");
+}
---
+++ main.ts
@@ -0,0 +1,15 @@
+let alienColor ="red";
+
+if(alienColor == "green"){
+    console.log(`the player earned 5 points`);
+
+}else if (alienColor =="Yellow"){
+    console.log(`the player earned 10 points`)
+
+} else if( alienColor == "red"){
+    console.log(`the player earned 15 points`)
+}
+  
+ else{
+        console.log(`the player earned 0 points`);
+    }
